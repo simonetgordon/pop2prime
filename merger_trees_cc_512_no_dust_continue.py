caed6bbee599f7d91f7ecd717408f75d9fae15c8
Simone Gordon <sgordon@cuillin.roe.ac.uk>
2022-11-04 17:23:22 +0000

    generating list of random DM particle positions. Trying to overlay ~20,000 DM particles on a gas projection plot.

--- merger_trees_cc_512_no_dust_continue.py
+++ merger_trees_cc_512_no_dust_continue.py
@@ -1,17 +1,139 @@
 import yt
 import ytree
+from yt.extensions.astro_analysis.halo_analysis import HaloCatalog
+import os
+from os.path import exists
+import matplotlib.pyplot as plt
+import random
 
-a = ytree.load("/disk12/brs/pop2-prime/firstpop2_L2-Seed3_large/cc_512_no_dust_continue/merger_trees/p2p_nd/p2p_nd.h5")
+plot_halos = False
+plot_mass_z = False
+print_halo_attributes = False
+new_param = False
+plot_particles = False
+plot_dm_on_gas = True
+radius_kpc = 0.2
 
-# Find most massive halo                                                                                                                                                                                   
-mass_array = a["mass"]
+# load data
+root_dir = "/disk12/brs/pop2-prime/firstpop2_L2-Seed3_large/cc_512_no_dust_continue"
+a = ytree.load(os.path.join(root_dir, 'merger_trees/p2p_nd/p2p_nd.h5'))
+ds = yt.load(os.path.join(root_dir, "DD0560/DD0560"))
 
-# Index of most massive halo
-index = mass_array.argmax()
+# useful functions
+def most_massive_halo_tree(a, write_out=False):
+    """
+    Find most massive halo and return its tree object
+    """
+    my_tree = a[a["mass"].argmax()]
+    prog_mass = my_tree["prog", "mass"]
+    prog_redshift = my_tree["prog", "redshift"]
+    if write_out:
+        print("progenitor masses: ", prog_mass)
+        print("progenitor redshifts: ", prog_redshift)
+    return my_tree
 
-# Most massive halo tree called my_tree
-my_tree = a[index]
+def halo_attributes(arbor, ds):
+    """
+    Return mass, position and radius of halo at last snapshot
+    """
+    mass = arbor[0]["mass"].to('Msun')
+    pos = arbor[0]["position"].to('unitary')
+    pos = arbor.arr(pos.d, "unitary")
+    rad = arbor[0]["virial_radius"].to('unitary')
+    rad = arbor.quan(rad.d, "unitary")
+    return mass, pos, rad
 
-p = ytree.TreePlot(my_tree)
-p.min_mass_ratio = 0.1
-p.save('tree_most_massive_halo.png')
+""" Main """
+if __name__ == "__main__":
+    # load most massive halo tree object
+    if exists("tree_43576459"):
+        arbor = ytree.load("tree_43576459/tree_43576459.h5")
+    else:
+        arbor = ytree.load(most_massive_halo_tree(a).save_tree())
+
+    # print halo attributes
+    if print_halo_attributes:
+        print("==================================================")
+        print("At z = {}, time = {}".format(ds.current_redshift, ds.current_time.in_units("Myr")))
+        print("Halo mass:     ", halo_attributes(arbor, ds)[0])
+        print("Halo position: ", halo_attributes(arbor, ds)[1])
+        print("Halo radius:   ", halo_attributes(arbor, ds)[2])
+        print("==================================================")
+
+    #sp_halo.write_out("sphere_0.2kpc.txt") #  fields=[('nbody', 'particle_index')]
+    if new_param is False:
+        sphere_ds = yt.load("DD0560_sphere.h5")
+        ad = sphere_ds.all_data()
+        dm_ids = ad[("nbody", "particle_index")]
+        print("dm ids: ", dm_ids)
+        print("number of dm particles: ", len(dm_ids))
+    else: # make sphere
+        sp_halo = ds.sphere(halo_attributes(arbor, ds)[1], (radius_kpc, "kpc"))
+        fields = [('nbody', 'particle_index'), ('nbody', 'particle_type'), ('gas', 'temperature'), ('gas', 'density'),
+                  ("all", "particle_position_x"), ("all", "particle_position_y"), ("all", "particle_position_z"),
+                  ("all", "particle_position")]
+        fn = sp_halo.save_as_dataset(fields=fields) # save as dataset
+        dm_ids = sp_halo["particle_index"][sp_halo["particle_type"] == 1]
+        print("dm ids: ", dm_ids)
+        print("number of dm particles: ", len(dm_ids))
+
+
+    """ Plotting """
+
+    # plot halo mass vs z of my_tree
+    if plot_mass_z:
+        prog_mass = most_massive_halo_tree(a)["prog", "mass"]
+        prog_redshift = most_massive_halo_tree(a)["prog", "redshift"]
+
+        plt.ylabel(r'Halo Mass $M_\odot$')
+        plt.xlabel(r'Redshift z')
+        plt.semilogy(prog_redshift, prog_mass)
+        plt.savefig('mass_vs_redshift.png')
+
+        # merger tree plot
+        p = ytree.TreePlot(most_massive_halo_tree(a))
+        p.min_mass_ratio = 0.1
+        p.save('tree_most_massive_halo.png')
+
+
+    # Make plot of full simulation volume and circle halos
+    if plot_halos:
+        hc = HaloCatalog(data_ds=ds, finder_method='hop')
+        hc.create()
+
+        prj_halos = yt.ProjectionPlot(ds, 'z', 'all_cic', data_source=ds)
+        prj_halos.annotate_halos(hc)
+        prj_halos.save('ProjectionPlot_halos_circled.png')
+
+    if plot_particles:
+        if new_param:
+            data_source = sp_halo
+        else:
+            data_source = ds.sphere(halo_attributes(arbor, ds)[1], (radius_kpc, "kpc"))
+        p = yt.ParticleProjectionPlot(ds, "z", color="g", data_source=data_source)
+        p.set_unit("particle_mass", "Msun")
+        p.save('particle_plot.png')
+
+    if plot_dm_on_gas:
+        if not new_param:
+            sp_halo = ds.sphere(halo_attributes(arbor, ds)[1], (radius_kpc, "kpc"))
+        p = yt.ProjectionPlot(
+            ds,
+            "z",
+            [("gas", "density"), ("gas", "temperature")],
+            weight_field=("gas", "density"),
+            center=halo_attributes(arbor, ds)[1],
+            width=(3, "kpc"),
+        )
+        random.seed(0)
+        dm_pos_all = sp_halo[("all", "particle_position")][sp_halo["particle_type"] == 1]
+        dm_ids_all = sp_halo["particle_index"][sp_halo["particle_type"] == 1]
+        # i = random.randint(0, len(dm_ids)-1)
+        no_particles = int(len(dm_ids_all)*0.01)
+        print(no_particles)
+        dm_indices = random.sample(range(len(dm_ids_all)-1), no_particles)
+        dm_pos = [dm_pos_all[i] for i in dm_indices]
+        #dm_pos.save_as_dataset()
+        print("dm positions: ", dm_pos)
+        p.annotate_particles((1, 'kpc'), data_source=sp_halo)
+        p.save('dm_particles_on_gas.png')
